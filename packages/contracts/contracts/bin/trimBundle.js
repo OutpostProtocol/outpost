7a9b1a9c4fa2685e4fe435641c5a5810c796559e
Sam <hatem@clearrain.xyz>
2020-09-17 19:24:16 -0400

    refactor for multiple contracts

--- packages/contracts/contracts/bin/trimBundle.js
+++ packages/contracts/contracts/bin/trimBundle.js
@@ -6,15 +6,16 @@ const fs = require('fs')
 const path = require('path')
 
 const CONTRACT_PATH = path.resolve(__dirname, '../../build/community.js')
+const ERC20_CONTRACT = path.resolve(__dirname, '../../build/ERC20Community.js')
 
-let contractSrc = fs.readFileSync(CONTRACT_PATH, 'utf8')
+trim(CONTRACT_PATH)
+trim(ERC20_CONTRACT)
 
-contractSrc = trim(contractSrc)
+function trim (path) {
+  let contractSrc = fs.readFileSync(path, 'utf8')
 
-fs.writeFileSync(CONTRACT_PATH, contractSrc)
-
-function trim (contractSrc) {
   contractSrc = contractSrc.replace('Object.defineProperty(exports, \'__esModule\', { value: true });', '')
   contractSrc = contractSrc.replace('exports.handle = handle;', '')
-  return contractSrc
+
+  fs.writeFileSync(path, contractSrc)
 }
